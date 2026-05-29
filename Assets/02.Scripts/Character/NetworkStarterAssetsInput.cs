37f04a6843354ba16fd14404dedb30dd2842f25a
202070023 <kimyy631@naver.com>
2026-05-29 17:38:39 +0900

    Reapply "1"
    This reverts commit 2b44201952a5ea5b775be5fafec02f736cb13e28.

--- Assets/02.Scripts/Character/NetworkStarterAssetsInput.cs
+++ Assets/02.Scripts/Character/NetworkStarterAssetsInput.cs
@@ -1,32 +1,13 @@
-using Fusion;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class NetworkStarterAssetsInput : MonoBehaviour
 {
-    public static NetworkStarterAssetsInput Local;
-
     public Vector2 Move;
     public Vector2 Look;
 
     public bool Jump;
     public bool Sprint;
-
-    private NetworkObject _networkObject;
-
-    private void Awake()
-    {
-        _networkObject = GetComponent<NetworkObject>();
-    }
-
-    private void Update()
-    {
-        if (_networkObject != null &&
-            _networkObject.HasInputAuthority)
-        {
-            Local = this;
-        }
-    }
 
     private void Start()
     {
