fed7008eb8a92af13f645bb0e6fc3b8445765e79
202070023 <kimyy631@naver.com>
2026-06-15 17:36:09 +0900

    EnemyUI추가

---
+++ Assets/02.Scripts/YW_Game/EnemyHUD.cs
@@ -0,0 +1,11 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHUD : MonoBehaviour
+{
+    [SerializeField] Image enemyHpBar;
+    [SerializeField] TMP_Text enemyHpText;
+
+
+}
